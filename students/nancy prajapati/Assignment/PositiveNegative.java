d1b900643b1cb96cee57fb357ec343b19f46715d
punyaprasad suwal <punyaprasads@gmail.com>
2022-04-20 17:13:30 -0500

    First Assignment

---
+++ students/nancy prajapati/Assignment/PositiveNegative.java
@@ -0,0 +1,20 @@
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		if (a<0) {
+			System.out.print("the number is negative");}
+		else
+		System.out.print("the number is positive");}
+			
+		
+		
+	
+	
+		
+		
+
+	}
+
