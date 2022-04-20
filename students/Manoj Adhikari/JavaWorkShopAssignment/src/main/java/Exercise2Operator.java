fb7e3104fdd7e2e3c85f74e6736025c780a40eb2
Manoj Adhikari <manojadqa@gmail.com>
2022-04-19 20:48:34 -0500

    Java Assignment done

---
+++ students/Manoj Adhikari/JavaWorkShopAssignment/src/main/java/Exercise2Operator.java
@@ -0,0 +1,13 @@
+public class Exercise2Operator {
+    public static void main(String[] args) {
+        System.out.println(24+2);
+        System.out.println(24-2);
+        System.out.println(24*2);
+        System.out.println(24/2);
+        System.out.println(24%5);
+
+
+
+
+    }
+}
---
+++ students/Manoj Adhikari/JavaWorkShopAssignment/src/main/java/FirstAssignmentPrint.java
@@ -0,0 +1,9 @@
+public class FirstAssignmentPrint {
+    /**
+     * noureenshaik
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello \n  Noureen Shaik ");
+    }
+}
