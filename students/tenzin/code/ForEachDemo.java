6598d955bd9bc9b775459ecea164f2903134dbba
Tenzin  Dorjee <tenzindorjee@Tenzins-MBP.lan>
2022-04-22 16:19:28 -0400

    new codes added

---
+++ students/tenzin/code/ForEachDemo.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		int[] arr= {2,43,2342,234,23};
+		for(int number:arr) {
+			System.out.print(number+" ");
+		}
+		
+		
+	}
+}
