071853269f60641e1099b5f4420d532754b4b768
LalsaPandit <lalsa.pandit@gmail.com>
2022-04-21 22:44:23 -0400

    This are Homework

---
+++ students/lalsa pandit/patten1to5.java
@@ -0,0 +1,24 @@
+package fundamentals;
+
+public class patten1to5 {
+
+	public static void main(String[] args) {
+		System.out.println("Question no.3");
+		
+		int r=5;
+		for (int i=1;i<=r;i++)
+		{
+			for ( int j =1; j<=r; j++);
+			{
+				System.out.print(j);
+				
+				
+			}
+			System.out.println();
+		
+		}
+		
+
+	}
+
+}
