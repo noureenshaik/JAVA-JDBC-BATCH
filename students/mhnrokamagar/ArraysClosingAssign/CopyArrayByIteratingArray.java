0b32680c798dcac4e65831b32dc4b39dcf56d097
Mohan Roka Magar <99700867+mhnrokamagar@users.noreply.github.com>
2022-04-25 23:36:31 -0400

    Array closing Assignment

---
+++ students/mhnrokamagar/ArraysClosingAssign/CopyArrayByIteratingArray.java
@@ -0,0 +1,22 @@
+package array;
+import java.util.Arrays;
+
+public class CopyArrayByIteratingArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
+		   int[] new_array = new int[10];     
+		   
+		   System.out.println("Source Array : " + Arrays.toString(my_array));
+		   
+		   for(int i = 0; i < my_array.length; i++) {
+			   new_array[i] = my_array[i];
+		   }
+		   
+		   System.out.println("New Array: " + Arrays.toString(new_array));
+		   
+
+	}
+
+}
