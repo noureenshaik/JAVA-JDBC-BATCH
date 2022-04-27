dcaeafa8d6e061c684a772ed12414b9312bcd6e1
rameee-maharjan <rameee.maharjan@gmail.com>
2022-04-26 22:23:15 -0500

    for loop exercise added

---
+++ students/ramita maharjan/forloop/JavaBreak.java
@@ -0,0 +1,17 @@
+package forloop;
+
+public class JavaBreak {
+	public static void main(String[] args) {
+	      
+        // for loop
+        for (int i = 1; i <= 10; ++i) {
+
+            // if the value of i is 5 the loop terminates  
+            if (i == 5) {
+                break;
+            }      
+            System.out.println(i);
+        }   
+    }
+
+}
