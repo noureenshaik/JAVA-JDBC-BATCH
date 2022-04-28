cb2795652430ca35ae32aac28f9fc34baa603b4a
LalsaPandit <lalsa.pandit@gmail.com>
2022-04-27 23:37:17 -0400

    4_27 assignments

---
+++ students/lalsa pandit/String_length.java
@@ -0,0 +1,15 @@
+
+public class String_length {
+
+	public static void main(String[] args) {
+		
+		String greet = "Hello World";
+		System.out.println("String: " + greet);
+		
+		// get the length of the greet
+		int length = greet.length();
+		System.out.println("Length: " + length);
+
+	}
+
+}
