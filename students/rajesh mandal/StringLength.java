9742dca1e8aa9b771a643e344a178a2114174081
Rajesh Mandal <rmandal.cs@gmail.com>
2022-04-27 22:29:00 -0400

    strings Assignment added

---
+++ students/rajesh mandal/StringLength.java
@@ -0,0 +1,14 @@
+package strings;
+
+class StringLength {
+	  public static void main(String[] args) {
+
+	    // create a string
+	    String greet = "Hello! World";
+	    System.out.println("String: " + greet);
+
+	    // get the length of greet
+	    int length = greet.length();
+	    System.out.println("Length: " + length);
+	  }
+	}
