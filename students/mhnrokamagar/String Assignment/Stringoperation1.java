59596013491df8bdb1225bc80ba0abf46c661570
Mohan Roka Magar <99700867+mhnrokamagar@users.noreply.github.com>
2022-04-27 22:49:49 -0400

    String Assignment April27

---
+++ students/mhnrokamagar/String Assignment/Stringoperation1.java
@@ -0,0 +1,15 @@
+package string;
+
+
+
+public class Stringoperation1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String m="Mohan";
+		System.out.println(m.toUpperCase());//MOHAN
+		System.out.println(m);//Mohan (no change in Original
+	}
+
+}
