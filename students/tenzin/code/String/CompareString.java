bc75b1332661571e836f82416db3d9a319bf3643
Tenzin  Dorjee <tenzindorjee@Tenzins-MBP.lan>
2022-04-27 23:03:55 -0400

    String Assignment

---
+++ students/tenzin/code/String/CompareString.java
@@ -0,0 +1,15 @@
+
+public class CompareString {
+
+	public static void main(String [] args) {
+		String firstName="Tenzin";
+		String secondName="Tenzin";
+		String thirdName="Tashi";
+		
+		boolean result1=firstName.equals(secondName);
+		System.out.println("The Result Of First And Second Name Is "+result1);
+		boolean result2=firstName.equals(thirdName);
+		System.out.println("The Result Of First And Second Name Is "+ result2);
+		
+	}
+}
