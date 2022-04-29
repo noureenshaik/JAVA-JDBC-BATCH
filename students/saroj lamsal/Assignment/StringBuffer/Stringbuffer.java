b00cec17442c752bd25b7ffca5f5858f069c5428
saroj <saroj.lamsal@gmail.com>
2022-04-29 18:48:16 -0400

    stringbuffer

---
+++ students/saroj lamsal/Assignment/StringBuffer/Stringbuffer.java
@@ -0,0 +1,12 @@
+package workshop;
+
+public class Stringbuffer {
+
+	public static void main(String[] args) {
+		StringBuffer sb=new StringBuffer("Hello ");
+		sb.append("Java");//now original string is changed
+		System.out.println(sb);//prints Hello Java
+
+	}
+
+}
