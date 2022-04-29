a541cc92feab43df3c6a55252c26383534c97911
Manoj Adhikari <manojadqa@gmail.com>
2022-04-28 21:51:03 -0500

    Added StringBuffer and StringBuilder Assignment

---
+++ students/Manoj Adhikari/JavaWorkShopAssignment/src/main/java/eighthStringBuffer/StringBufferEx.java
@@ -0,0 +1,9 @@
+package eighthStringBuffer;
+
+public class StringBufferEx {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello");
+        sb.append(" Manoj ");
+        System.out.println(sb);
+    }
+}
---
+++ students/Manoj Adhikari/JavaWorkShopAssignment/src/main/java/ninthStringBuilder/SringBufferInsert.java
@@ -0,0 +1,11 @@
+package ninthStringBuilder;
+
+public class SringBufferInsert {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("hello");
+        sb.insert(1,"Ram");
+        System.out.println(sb);
+
+
+    }
+}
