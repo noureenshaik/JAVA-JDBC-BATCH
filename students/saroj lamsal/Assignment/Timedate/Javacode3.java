1b2ad6ba2a89453e2c3d75fd5b0cac2c52684164
saroj <saroj.lamsal@gmail.com>
2022-05-01 16:21:29 -0400

    finished

---
+++ students/saroj lamsal/Assignment/Timedate/Javacode3.java
@@ -0,0 +1,21 @@
+package workshop;
+
+import java.util.*;
+
+public class Javacode3 {
+
+	public static void main(String[] args) {
+	//two weeks
+		int noOfDays = 14;
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_YEAR, noOfDays);
+		Date date = cal.getTime();
+		String cdate = null;
+		System.out.println("\nCurrent Date:" +cdate+"\n");
+		System.out.println("Day after two weeks:" +date+"\n");
+				
+		
+
+	}
+
+}
