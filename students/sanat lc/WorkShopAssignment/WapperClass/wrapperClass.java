6a63aac59a282853557f2247a56be14eab7f664a
slc_Github <sanat.lami@gmail.com>
2022-05-02 18:41:59 -0500

    Wapperclass

---
+++ students/sanat lc/WorkShopAssignment/WapperClass/wrapperClass.java
@@ -0,0 +1,19 @@
+package com.WapperClass;
+
+public class wrapperClass {
+	
+	//Wrapper class Example: Primitive to Wrapper
+	public class WrapperExample1{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// Wrapper class Example: Primitive to Wrapper
+		int a=20;
+	Integer i= Integer.valueOf(a);
+	Integer j=a;
+	System.out.println(a+" "+i+" "+j);
+	}
+
+}
