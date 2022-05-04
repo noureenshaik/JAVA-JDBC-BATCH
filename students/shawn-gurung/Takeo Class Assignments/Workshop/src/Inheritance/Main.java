b2655fb6ce5f2a2431244ea00f58bec258b33ccb
gurungs013 <sushang41@gmail.com>
2022-05-03 23:14:01 -0400

    Inheritance

---
+++ students/shawn-gurung/Takeo Class Assignments/Workshop/src/Inheritance/Main.java
@@ -0,0 +1,28 @@
+package Inheritance;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+ 
+class one {
+    public void print_geek()
+    {
+        System.out.println("Geeks");
+    }
+}
+ 
+class two extends one {
+    public void print_for()
+ {
+ System.out.println("for"); 
+}
+}
+// Driver class
+public class Main {
+    public static void main(String[] args)
+    {
+        two g = new two();
+        g.print_geek();
+        g.print_for();
+        g.print_geek();
+    }
+}
