cfad7f548de43047c832bbb7e18f9c6cac28d52a
Mohan Roka Magar <99700867+mhnrokamagar@users.noreply.github.com>
2022-05-04 13:27:18 -0400

    Inheritance Assignmnt may3

---
+++ students/mhnrokamagar/Inheritance Assignment/MultilevelInheritance.java
@@ -0,0 +1,36 @@
+package com.employee.inheri;
+
+class Animal {
+	void eat() {
+		System.out.println("eating...");
+	}
+}
+
+class Dog extends Animal {
+	void bark() {
+		System.out.println("barking...");
+	}
+}
+
+class BabyDog extends Dog {
+	void weep() {
+		System.out.println("weeping...");
+	}
+}
+
+public class MultilevelInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BabyDog d = new BabyDog();
+		d.weep();
+		d.bark();
+		d.eat();
+
+	}
+
+}
+
+//output:   weeping...
+          //barking....
+        ///eating...
