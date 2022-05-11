60161b1286ff0d44c1670718f4ae50b26d87cc61
Manoj Adhikari <manojadqa@gmail.com>
2022-05-10 21:12:24 -0500

    Interview question -5 and classwor

---
+++ students/Manoj Adhikari/OopsAssignment/src/Interview/ReverseWithoutStringMethod.java
@@ -0,0 +1,12 @@
+package Interview;
+
+public class ReverseWithoutStringMethod {
+	public static void main(String[] args) {
+		String str = "Automation";
+		StringBuilder str1 = new StringBuilder();
+		str1.append(str);
+		str1 = str1.reverse();
+		System.out.println(str1);
+	}
+
+}
