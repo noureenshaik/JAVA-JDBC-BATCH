85c5c00587cc64654bfb27638112096f5f83b1d2
rameee-maharjan <rameee.maharjan@gmail.com>
2022-05-11 21:53:01 -0500

    exercise files added

---
+++ students/ramita maharjan/exercisemay10/InbuiltReverse.java
@@ -0,0 +1,9 @@
+package exercisemay10;
+
+public class InbuiltReverse {
+	public static void main(String args[]) {
+	String st = "straight";
+	StringBuilder sb = new StringBuilder(st);
+	System.out.println("Reverse of string tram: " + sb.reverse());
+	}
+}
