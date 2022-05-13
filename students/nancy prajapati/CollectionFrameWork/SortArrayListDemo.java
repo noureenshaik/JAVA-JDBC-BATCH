9d5a967ae2447af327846910be177f3b7ebc6d2f
punyaprasad suwal <punyaprasads@gmail.com>
2022-05-12 21:19:37 -0500

    CollectionFrameWork

---
+++ students/nancy prajapati/CollectionFrameWork/SortArrayListDemo.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list1=new ArrayList<String>();
+		list1.add("Mango");
+		list1.add("Apple");
+		list1.add("Banana");
+		list1.add("Grapes");
+		//Sorting a list of fruit
+		Collections.sort(list1);
+		System.out.println(list1);
+		
+		//Traversing list through the for -each loop
+		for(String fruit:list1)
+			System.out.println(fruit);
+		
+		System.out.println("Sorting order is:");
+		//Creating a list of numbers
+		List<Integer>list2=new ArrayList<Integer>();
+		
+		list2.add(21);
+		list2.add(11);
+		list2.add(51);
+		list2.add(1);
+		//Sorting the list
+		Collections.sort(list2);
+		System.out.println(list2);
+		//Traversing list through the for -each loop
+		for(Integer number:list2)
+			System.out.println(number);
+		
+		
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
